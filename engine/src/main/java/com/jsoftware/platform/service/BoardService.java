ce7cd5c3ff4ca6bbd02ea1d9c5e83abbe63eb358
john <q7phavoc2@gmail.com>
2021-07-27 00:00:59 +0900

    게시판 및 페이징 처리 기본

--- engine/src/main/java/com/jsoftware/platform/service/BoardService.java
+++ engine/src/main/java/com/jsoftware/platform/service/BoardService.java
@@ -1,18 +1,22 @@
 package com.jsoftware.platform.service;
 
 import com.jsoftware.platform.model.Board;
-import com.jsoftware.platform.repository.BoardRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.jsoftware.platform.model.Product;
+import com.jsoftware.platform.repository.BoardRepositoryImpl;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class BoardService {
 
-    @Autowired
-    BoardRepository repository;
+    final BoardRepositoryImpl repository;
+
+    public BoardService(BoardRepositoryImpl repository) {
+        this.repository = repository;
+    }
 
     @Cacheable(key = "#size", value = "getBoards")
     public List<Board> getBoards(String size) {
@@ -20,6 +24,31 @@ public class BoardService {
     }
 
     public static int getDbCount() {
-        return BoardRepository.getDbCount();
+        return BoardRepositoryImpl.getDbCount();
+    }
+
+    public Board getBoardById(Long id) {
+        Board board = new Board();
+        board.setId(id);
+        return repository.selectBoardById(board);
+    }
+
+    public List<Board> getAllBoards() {
+        return repository.selectAllBoards();
+    }
+
+    @Transactional
+    public void addBoard(Board board) {
+        repository.insertBoard(board);
+    }
+
+    @Transactional
+    public void updateBoard(Board board) {
+        repository.updateBoard(board);
+    }
+
+    @Transactional
+    public void deleteBoard(Board board) {
+        repository.deleteBoard(board);
     }
 }
